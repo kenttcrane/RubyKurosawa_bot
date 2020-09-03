6377ad7e7f55e1e39fa340b0a4a6673937a2f9bc
kenttcrane <55525930+kenttcrane@users.noreply.github.com>
2020-09-04 00:34:43 +0900

    Add files via upload

--- main_PC.rb
+++ main_PC.rb
@@ -5,44 +5,34 @@ require './hello.rb'
 require './bye.rb'
 require './message.rb'
 
-def reply(replies) #リプライを行う、70秒程度かかる
-	prev_replies = Marshal.load(Marshal.dump(replies)) #深いコピーを行っている。もともと取得していたリプライのArrayをprev_repliesに代入している
-	sleep 70 #70秒待機
-	replies.replace($client.mentions) #repliesの更新
-	
-	puts "リプライを更新しました"
+def reply() #リプライを行う、70秒程度かかる
 
-	(0...replies.size).reverse_each do |i| #古い順に返信をしていく
-		if (!(prev_replies.include?(replies[i])))&&(replies[i].user.screen_name!=$my_id) then #古いリプライArrayに含まれておらず、自分によるリプライでなければ、それにリプライする
-			puts replies[i].text
-			$client.update("@"+replies[i].user.screen_name+"\n"+message(replies[i]),{:in_reply_to_status_id => replies[i].id})
+	#replied_idは、自分の最新100ツイートに含まれるリプライのリプライ先のツイートid
+	replied_id = $client.user_timeline($my_id,{:count => 100}).map{|x| x.in_reply_to_status_id} #削除されたツイートがあるとエラーになるのでidの状態で比較する
+	replied_id.delete_if do |r|
+		r.class != Integer
+	end
+
+	#repliesは、自分宛てのリプライ最新20件のうち、24時間以内に受け取ったもの
+	replies = $client.mentions
+	for i in 0...replies.size
+		if replies[i].created_at < Time.now - 24 * 60 * 60
+			replies.replace(replies[0...i])
+			break
 		end
 	end
-end
 
-def favorite(tweets,fav_words) #いいねを行う、70秒程度かかる
-	prev_tweets = Marshal.load(Marshal.dump(tweets)) #深いコピーを行っている
-	sleep 70
-	tweets.replace($client.home_timeline)
-	
-	puts "ツイート一覧を更新しました"
-
-	(0...tweets.size).reverse_each do |i| #古い順にいいねしていく
-		if prev_tweets.include?(tweets[i]) #古いツイートArrayに含まれているならば飛ばす
-			next
-		end
-
-		for word in fav_words do #いいねの対象となるフレーズが含まれているか各正規表現ごとに調べ、含まれているかつフォロワーのツイートであるならばいいねする
-			if tweets[i].text.match(word) != nil && $client.friendship?(tweets[i].user.screen_name, $my_id) then
-				puts tweets[i].text
-				$client.favorite(tweets[i].id)
-				break
-			end
+	#古い順にリプライしていく
+	for reply in replies.reverse
+		if (!replied_id.include?(reply.id)) && (reply.user.screen_name!=$my_id)
+			puts reply.text
+			$client.update("@"+reply.user.screen_name+"\n"+message(reply),{:in_reply_to_status_id => reply.id})
 		end
 	end
+
 end
 
-def isActive?() #起きている時間かどうかをboolで返す
+def active?() #起きている時間かどうかをboolで返す
 	return (4<Time.now.hour && Time.now.hour<23)
 end
 
@@ -50,9 +40,6 @@ $client = my_client()
 
 #変数設定、$で始まるのはグローバル変数
 $my_id = $client.user.screen_name #自分のIDから@を除いた文字列
-replies = $client.mentions #自分へのリプライを最新20件のみ保存
-tweets = $client.home_timeline #ホームの最新のツイート一覧から20個とってくる
-fav_words = [/[うぅ][ゆゅ]/,/るびぃ/,/ルビィ/,/[Rr]uby/,/ラブライ/] #正規表現、いいねするツイートに含まれる語句
 go_to_bed = false #おやすみツイートをするかどうか
 
 puts "セットアップ完了"
@@ -60,15 +47,15 @@ puts "セットアップ完了"
 #処理
 
 #もし起きている時間帯で、まだおはようツイートをしていなければ、おはようツイートをする
-if isActive?() && !($client.user_timeline($my_id,{:count => 100}).map(&:text).include?(hello())) then
+if active?() && !($client.user_timeline($my_id,{:count => 100}).map(&:text).include?(hello())) then
 	$client.update(hello())
 end
 
 #起きている間、リプライといいねを繰り返す
-while isActive?() do
+while active?() do
 	go_to_bed = true
-	reply(replies)
-	favorite(tweets,fav_words)
+	sleep 5 * 60
+	reply()
 end
 
 #もし寝る時間になったらおやすみツイートをする
--- main_PC_Arduino.rb
+++ main_PC_Arduino.rb
@@ -7,47 +7,38 @@ require './hello.rb'
 require './bye.rb'
 require './message.rb'
 
-def reply(replies) #新着リプライにリプライを行う。1回でも行えばtrueを、そうでなければfalseを返す
+def reply() #新着リプライにリプライを行う。1回でも行えばtrueを、そうでなければfalseを返す
 	update = false #リプライに更新があったかどうか
-	prev_replies = Marshal.load(Marshal.dump(replies)) #深いコピーを行う
-	sleep 70
-	replies.replace($client.mentions)
 
-	puts "リプライを更新しました"
+	#replied_idは、自分の最新100ツイートに含まれるリプライのリプライ先のツイートid
+	replied_id = $client.user_timeline($my_id,{:count => 100}).map{|x| x.in_reply_to_status_id} #削除されたツイートがあるとエラーになるのでidの状態で比較する
+	replied_id.delete_if do |r|
+		r.class != Integer
+	end
 
-	(0...replies.size).reverse_each do |i|
-		if (!(prev_replies.include?(replies[i])))&&(replies[i].user.screen_name!=$my_id) then
-			update = true
-			puts replies[i].text
-			$client.update("@"+replies[i].user.screen_name+"\n"+message(replies[i]),{:in_reply_to_status_id => replies[i].id})
+	#repliesは、自分宛てのリプライ最新20件のうち、24時間以内に受け取ったもの
+	replies = $client.mentions
+	for i in 0...replies.size
+		if replies[i].created_at < Time.now - 24 * 60 * 60
+			replies.replace(replies[0...i])
+			break
 		end
 	end
+
+	#古い順にリプライしていく
+	for reply in replies.reverse
+		if (!replied_id.include?(reply.id)) && (reply.user.screen_name!=$my_id)
+			update = true
+			puts reply.text
+			$client.update("@"+reply.user.screen_name+"\n"+message(reply),{:in_reply_to_status_id => reply.id})
+		end
+	end
+
 	return update
 end
 
-def favorite(tweets,fav_words)#関連ワードを含んだ新着ツイートにいいねを押す
-	prev_tweets = Marshal.load(Marshal.dump(tweets)) #深いコピーを行う
-	sleep 70
-	tweets.replace($client.home_timeline)
-	
-	puts "ツイート一覧を更新しました"
 
-	(0...tweets.size).reverse_each do |i|
-		if prev_tweets.include?(tweets[i])
-			next
-		end
-
-		for word in fav_words do
-			if tweets[i].text.match(word) != nil && $client.friendship?(tweets[i].user.screen_name, $my_id) then
-				puts tweets[i].text
-				$client.favorite(tweets[i].id)
-				break
-			end
-		end
-	end
-end
-
-def isActive?() #起きている時間かどうか判定を行う
+def active?() #起きている時間かどうか判定を行う
 	return (4<Time.now.hour && Time.now.hour<23)
 end
 
@@ -55,9 +46,6 @@ $client = my_client()
 
 #変数設定、$で始まるのはグローバル変数
 $my_id = $client.user.screen_name #自分のIDから@を除いた文字列
-replies = $client.mentions #最新20件のみ保存、ゆえに現在のsizeは20固定
-tweets = $client.home_timeline() #最新のものから20個とってくる
-fav_words = [/[うぅ][ゆゅ]/,/るびぃ/,/ルビィ/,/[Rr]uby/,/ラブライ/] #正規表現
 go_to_bed = false
 
 sp = Serial.new('COM3', 9600) #device,とrate。必要に応じて変える
@@ -74,21 +62,24 @@ while true
 	sleep 5
 end
 
-if isActive?() && signal.chomp=="morning" && !($client.user_timeline($my_id,{:count => 100}).map(&:text).include?(hello()))
+#起きている時間帯に"morning"を受け取り、かつ同様のツイートをしていなければおはようツイートをする
+if active?() && signal.chomp=="morning" && !($client.user_timeline($my_id,{:count => 100}).map(&:text).include?(hello()))
 	$client.update(hello())
 end
 
-while isActive?() do
+#5分おきに返信
+while active?() do
 	go_to_bed = true
-	if reply(replies)
-		sp.write "reply"
+	sleep 5 * 60
+	if reply()
+		sp.write "reply" #シリアル通信
 	end
-	favorite(tweets,fav_words)
 end
 
+#おやすみツイートをする
 if go_to_bed then
 	$client.update(bye())
 	sp.write "sleep"
+	puts "bye"
+	fin = gets
 end
-
-
